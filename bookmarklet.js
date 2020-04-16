4a7413a8e5eb7c2970623511f500350493930a2d
Sascha Brunner <46531184+SaschaBrunnerCH@users.noreply.github.com>
2020-04-16 08:39:05 +0200

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,8 @@
 console.log("Bookmarklet");
+require(["esri/views/View","esri/widgets/Home"], function (View,Home) {
+	const view = View.views.getItemAt(0);
+	var homeBtn = new Home({
+	  view: view
+	});
+	view.ui.add(homeBtn, "top-left");
+});
